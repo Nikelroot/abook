2810c15a665b39174d5eb0ef2ed3c3e9cb50c522
Nikelroot <nikel.root@gmail.com>
2026-03-05 15:30:25 +0000

    1

--- start.sh
+++ start.sh
@@ -2,6 +2,15 @@
 
 sh /mnt/W/projects/create_network.sh
 sh /mnt/W/projects/auth/start.sh
+sh /mnt/W/projects/cdn/start.sh
 
-docker compose -f /mnt/W/projects/abook/docker-compose.yml up -d
+docker compose -f /mnt/W/projects/abook/docker-compose.yml up -d abook_vless
+docker compose -f /mnt/W/projects/abook/docker-compose.yml up -d mongo_abook
+docker compose -f /mnt/W/projects/abook/docker-compose.yml up -d abook_api
+docker compose -f /mnt/W/projects/abook/docker-compose.yml up -d abook_web
+docker compose -f /mnt/W/projects/abook/docker-compose.yml up -d abook_parser
+# docker compose -f /mnt/W/projects/abook/docker-compose.yml up -d abook_parser_link
+# docker compose -f /mnt/W/projects/abook/docker-compose.yml up -d abook_parser_metadata
+# docker compose -f /mnt/W/projects/abook/docker-compose.yml up -d qbook_qbt
+# docker compose -f /mnt/W/projects/abook/docker-compose.yml up -d abook_qbittorrent
 
